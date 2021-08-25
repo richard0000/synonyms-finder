f3543d2c9ef23b23f49543562a6f8d502a548b21
Ricardo Gamarra <r.richard0000@gmail.com>
2021-08-25 10:08:24 -0300

    Fix 'page refresh needed' issue
    As per the documentation on the GM_RegisterMenuCommand, the name of the function to be registered has to be just the reference, not the execution of the function.
    Also add readme and format the code.

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Synonyms Finder
+
+### Description
+
+A script that will let you find word synonyms from text from all over the web.
+
+### How to use
+
+- An extension like TamperMonkey will be needed.
+- Just install this script using the mentioned extension.
+- Then, while navigating, select a word and right click over it, select the TamperMonkey -> Synonyms Finder -> Find Synonyms option
+
+Enjoy!
--- Synonym Finder.user.js
+++ Synonym Finder.user.js
@@ -1,98 +1,96 @@
 // ==UserScript==
+// @author       Ricardo Gamarra <r.richard0000@gmail.com>
+// @description  Let the user to select a word (without spaces) anywhere in the DOM, and gives an option in the contextual menu to go for synonyms over the web
 // @name         Synonym Finder
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Let the user to select a word (without spaces) anywhere in the DOM, and gives an option in the contextual menu to go for synonyms over the web
-// @author       Ricardo Gamarra <r.richard0000@gmail.com>
-// @match        http*://*/*
-// @icon         https://cdn1.iconfinder.com/data/icons/web-tech-misc/80/duplicates-emulate-emulation-synonym-512.png
 // @grant        GM_registerMenuCommand
 // @grant        GM_xmlhttpRequest
-// @run-at          context-menu
+// @grant        GM_openInTab
+// @grant        GM.openInTab
+// @match        http*://*/*
+// @run-at       context-menu
+// @version      1.0
 // ==/UserScript==
 
-(function () {
-  "use strict";
+;(function () {
+  'use strict'
 
   /**
    * Finds a synonym for a given word
    */
   class SynonymsFinder {
     constructor(word) {
-      this.word = word;
+      this.word = word
     }
 
     find() {
       return new Promise((resolve, reject) => {
         GM_xmlhttpRequest({
-          method: "GET",
+          method: 'GET',
           url: `https://www.thesaurus.com/browse/${this.word}`,
           onload: function (response) {
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(
-              response.responseText,
-              "text/html"
-            );
+            let parser = new DOMParser()
+            let doc = parser.parseFromString(response.responseText, 'text/html')
             let synonymContainers = doc.getElementsByClassName(
-              "css-1kg1yv8 eh475bn0"
-            );
-            let synonyms = [...synonymContainers].map((elem) => elem.innerText);
-            resolve(synonyms);
+              'css-1kg1yv8 eh475bn0'
+            )
+            let synonyms = [...synonymContainers].map((elem) => elem.innerText)
+            resolve(synonyms)
           },
           onerror: function (error) {
-            reject(error);
-          },
-        });
-      });
+            reject(error)
+          }
+        })
+      })
     }
   }
 
   class SynonymsRenderer {
     constructor(synonyms, mainWord) {
-      this.synonyms = synonyms;
-      this.mainWord = mainWord;
+      this.synonyms = synonyms
+      this.mainWord = mainWord
     }
 
     capitalizeFirstLetter(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
+      return string.charAt(0).toUpperCase() + string.slice(1)
     }
 
     titleDiv(popup) {
-      let titleDiv = popup.document.createElement("div");
+      let titleDiv = popup.document.createElement('div')
       titleDiv.style =
-        "text-align:center;font-family:Courier, monospace;color:#3e3e3e;font-weight:bold;text-shadow: 3px 3px 10px #5D4037";
-      let title = popup.document.createElement("h1");
-      title.innerHTML = `${this.synonyms.length} synonyms found for "${this.mainWord}"`;
-      titleDiv.appendChild(title);
+        'text-align:center;font-family:Courier, monospace;color:#3e3e3e;font-weight:bold;text-shadow: 3px 3px 10px #5D4037'
+      let title = popup.document.createElement('h1')
+      title.innerHTML = `${this.synonyms.length} synonyms found for "${this.mainWord}"`
+      titleDiv.appendChild(title)
 
-      return titleDiv;
+      return titleDiv
     }
 
     listDiv(popup) {
-      let listDiv = popup.document.createElement("div");
+      let listDiv = popup.document.createElement('div')
       listDiv.style =
-        "border-radius:5px;background-color:#455A64;color:#fff;padding:20px;font-family:Courier, monospace;font-size:16px";
-      let list = popup.document.createElement("ul");
+        'border-radius:5px;background-color:#455A64;color:#fff;padding:20px;font-family:Courier, monospace;font-size:16px'
+      let list = popup.document.createElement('ul')
       this.synonyms.forEach((synonym) => {
-        let item = popup.document.createElement("li");
-        item.innerHTML = this.capitalizeFirstLetter(synonym);
-        list.appendChild(item);
-      });
-      listDiv.appendChild(list);
+        let item = popup.document.createElement('li')
+        item.innerHTML = this.capitalizeFirstLetter(synonym)
+        list.appendChild(item)
+      })
+      listDiv.appendChild(list)
 
-      return listDiv;
+      return listDiv
     }
 
     render() {
       let popup = open(
-        "",
-        "Popup",
-        "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400;"
-      );
+        '',
+        'Popup',
+        'toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400;'
+      )
 
-      popup.document.body.innerHTML = "";
-      popup.document.body.appendChild(this.titleDiv(popup));
-      popup.document.body.appendChild(this.listDiv(popup));
+      popup.document.body.innerHTML = ''
+      popup.document.body.appendChild(this.titleDiv(popup))
+      popup.document.body.appendChild(this.listDiv(popup))
     }
   }
 
@@ -102,32 +100,31 @@
    */
   class SynonymAugmenter {
     constructor() {
-      this.userSelection = this.getUserSelection();
+      this.userSelection = this.getUserSelection()
     }
 
     getUserSelection() {
-      return window.getSelection().toString();
+      return window.getSelection().toString()
     }
 
     augment() {
-      if (this.userSelection == "") {
-        alert("Please, select only one word to let me find synonyms");
-        return;
+      if (this.userSelection == '') {
+        alert('Please, select only one word to let me find synonyms')
+        return
       }
 
-      let finder = new SynonymsFinder(this.userSelection);
+      let finder = new SynonymsFinder(this.userSelection)
       finder.find().then((results) => {
-        let renderer = new SynonymsRenderer(results, this.userSelection);
-        renderer.render();
-      });
+        let renderer = new SynonymsRenderer(results, this.userSelection)
+        renderer.render()
+      })
     }
   }
 
-  GM_registerMenuCommand("Search Synonyms", triggerAugmenter(), "S");
+  GM_registerMenuCommand('Search Synonyms', triggerAugmenter, 'S')
 
   function triggerAugmenter() {
-    console.log("triggering augmenter");
-    let augmenter = new SynonymAugmenter();
-    return augmenter.augment();
+    let augmenter = new SynonymAugmenter()
+    return augmenter.augment()
   }
-})();
+})()
